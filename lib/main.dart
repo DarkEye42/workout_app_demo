dee3f0b07b5d367b0c0e0e81739c301371fe40a9
Nazmul Islam <nir71.ni@gmail.com>
2024-04-02 21:50:45 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Hello World'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
